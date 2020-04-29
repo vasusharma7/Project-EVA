8647f0ab85244a6d540cc006ae668af1d7678105
vasvi1203 <64128681+vasvi1203@users.noreply.github.com>
2020-04-29 13:08:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,6 +61,26 @@ while 1:
         for j in search(text2, tld="co.in", num=10,stop=10,pause=2):
             if "agoodmovietowatch.com" in j:
                 break
+    elif "camera" in text:
+        cam = cv2.VideoCapture(0)
+        cv2.namedWindow("test")
+        img_counter = 0
+        while True:
+            ret, frame = cam.read()
+            cv2.imshow("test", frame)
+            if not ret:
+                break
+            k = cv2.waitKey()
+            if k%256 == 27:
+                print("Escape hit, closing...")
+                break
+            elif k%256 ==32:
+                img_name = "opencv_frame_{}.png".format(img_counter)
+                cv2.imwrite(img_name, frame)
+                print("{} saved".format(img_name))
+                img_counter += 1
+        cam.release()
+        cv2.destroyAllWindows()
     elif "exit" in text:
         print("GoodBye")
         break
