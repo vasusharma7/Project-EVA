7b17243a6d94acda81aa048d0cdc28728dc1db50
Soham Parekh <soham.parekh2010@gmail.com>
2020-05-20 20:08:18 +0530

    Removing GUI

--- EVA-win32-x64/resources/app/ppl/hello.py
+++
@@ -1,5 +0,0 @@
-import sys
-import time
-print('Hello from Python!')
-time.sleep(3)
-print('Hello from Python!')
--- EVA-win32-x64/resources/app/ppl/temp.js
+++
@@ -1,8 +0,0 @@
-sum(10, 20);
-diff(40, 50);
-function sum(x, y) {
-  return x + y;
-}
-let diff = function (x, y) {
-  return x - y;
-};
