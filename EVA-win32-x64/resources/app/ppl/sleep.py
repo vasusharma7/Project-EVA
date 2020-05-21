58436b53e98e758ef9f5d77781c5ff09a2f57690
Soham Parekh <54930568+parekh0711@users.noreply.github.com>
2020-05-21 13:13:04 +0530

    Update sleep.py

--- EVA-win32-x64/resources/app/ppl/sleep.py
+++ EVA-win32-x64/resources/app/ppl/sleep.py
@@ -1,14 +1,17 @@
-from modules import*
+from modules import *
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
     audio = r.listen(source)
+
 try:
     text = r.recognize_google(audio)
     text = text.lower()
+    if text == "pluto":
+        print("activate")
+    else:
+        print("deactivate")
 except:
+    print("error")
     sys.exit(0)
-if text=="wake up":
-    print("Activate")
-else:
-    print("Sleep")
